04efd31ecfa943a2ef9006383cbd919a71db72cf
ajayzacharia <ajayzacharia94@gmail.com>
2024-02-13 21:37:11 +0530

    firstcommit

---
+++ src/test/java/mavennewpack/Newone.java
@@ -0,0 +1,13 @@
+package mavennewpack;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Newone {
+
+	public static void main(String[] args) {
+	ChromeDriver d=new ChromeDriver();
+	d.get("https://www.facebook.com/");
+	System.out.println("ss");
+	}
+
+}
